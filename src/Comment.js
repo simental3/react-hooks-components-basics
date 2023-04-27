728691420e9af5cdb6bd5bd7703bfad13d314f49
Anthony Simental <anthonysimental12@gmail.com>
2023-04-27 12:38:22 -0700

    commit tree 4a6b3ccc6938ed1ed72956601244d330e449f004

--- src/Comment.js
+++ src/Comment.js
@@ -5,3 +5,15 @@ function Comment() {
 }
 
 export default Comment
+
+/*
+// Arrow function:
+const Comment = () => <div>Naturally, I agree with this article.</div>
+
+// Class component:
+class Comment extends React.Component {
+  render() {
+    return <div>Naturally, I agree with this article.</div>
+  }
+}
+*/
